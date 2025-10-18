2c9dea1a8b9387f8c1501bdb8a6f96863fb781d4
MDecker-MobileComputing <mide_42b@yahoo.de>
2025-10-18 19:57:19 +0200

    Lauffaehige Version

---
+++ README.md
@@ -0,0 +1,50 @@
+# Python+Flask: BMI-Berechnung #
+
+<br>
+
+Dieses Repository enthält ein Python-Programm, das mit [Flask](https://flask.palletsprojects.com/en/stable/) einen REST-Endpunkt bereitstellt.
+
+<br>
+
+----
+
+## Anwendung ausführen ##
+
+<br>
+
+Abhängigkeiten installieren und Ausführen (Variante 1):
+```
+pip install -r requirements.txt
+```
+
+<br>
+
+Abhängigkeiten installieren und Ausführen (Variante 2):
+```
+py -m pip install -r requirements.txt
+```
+
+<br>
+
+Programm starten:
+```
+python bmi_app.py
+```
+
+<br>
+
+Beispielaufe bei lokaler Ausführung:
+* http://localhost:8080/bmi?kg=75&cm=180
+* http://localhost:8080/bmi?kg=110&cm=175
+
+<br>
+
+----
+
+## License ##
+
+<br>
+
+See the [LICENSE file](LICENSE.md) for license rights and limitations (BSD 3-Clause License).
+
+<br>
---
+++ bmi_app.py
@@ -0,0 +1,74 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+PORT_NUMMER = 8080
+
+HTTP_STATUS_BAD_REQUEST = 400
+HTTP_STATUS_OK = 200
+
+
+@app.route('/bmi')
+def bmi():
+    # URL-Parameter abfragen
+    kg_str = request.args.get('kg')
+    cm_str = request.args.get('cm')
+
+    if kg_str is None:
+        return jsonify({
+            'erfolg': False,
+            'nachricht': 'Bitte URL-Parameter >kg< angeben.'
+        }), HTTP_STATUS_BAD_REQUEST
+
+    if cm_str is None:
+        return jsonify({
+            'erfolg': False,
+            'nachricht': 'Bitte URL-Parameter >cm< angeben.'
+        }), HTTP_STATUS_BAD_REQUEST
+
+    # in ganze Zahlen umwandeln und validieren
+    try:
+        kg = int(kg_str)
+    except (ValueError, TypeError):
+        return jsonify({
+            'erfolg': False,
+            'nachricht': f'Wert "{kg_str}" für URL-Parameter >kg< ist keine Zahl.'
+        }), HTTP_STATUS_BAD_REQUEST
+
+    try:
+        cm = int(cm_str)
+    except (ValueError, TypeError):
+        return jsonify({
+            'erfolg': False,
+            'nachricht': f'Wert "{cm_str}" für URL-Parameter >cm< ist keine Zahl.'
+        }), HTTP_STATUS_BAD_REQUEST
+
+    # eigentliche Berechnung (cm -> m conversion handled same as JS: *10000)
+    bmi_value = kg / (cm * cm) * 10000
+
+    # auf eine Nachkommastelle runden
+    bmi_value = round(bmi_value * 10) / 10
+
+    if bmi_value < 18.5:
+        interpretation = 'Untergewicht'
+    elif bmi_value < 25.0:
+        interpretation = 'Normalgewicht'
+    elif bmi_value < 30.0:
+        interpretation = 'Prä-Adipositas'
+    elif bmi_value < 35.0:
+        interpretation = 'Moderate Adipositas'
+    elif bmi_value < 40.0:
+        interpretation = 'Starke Adipositas'
+    else:
+        interpretation = 'Extreme Adipositas'
+
+    return jsonify({
+        'erfolg': True,
+        'bmi': bmi_value,
+        'nachricht': interpretation
+    }), HTTP_STATUS_OK
+
+
+if __name__ == '__main__':
+    # Development server; use env/WSGI for production
+    app.run(port=PORT_NUMMER)
